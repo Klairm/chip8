c600ea30e84d75d19560349544daa69cbcf84a3d
Klairm <ivangardis@hotmail.com>
2019-10-23 16:50:16 +0200

    Reindented code

--- src/chip8.c
+++ src/chip8.c
@@ -443,64 +443,64 @@ int main (int argc,char ** argv)
 			
 			printf("Speed: %d \n",speed);
 			while(SDL_PollEvent(&event)){
-			switch(event.type)
-			{
-				case SDL_QUIT:
-				quit = 1;
-				break;
-
-				case SDL_KEYDOWN:
-
-				switch (event.key.keysym.sym)
+				switch(event.type)
 				{
-					case SDLK_ESCAPE:quit = 1;break;
-					case SDLK_F1:initChip8();loadRom();break;
-					case SDLK_F2:speed -= 1;break;
-					case SDLK_F3:speed += 1;break;
-					case SDLK_x:keyboard[0] = 1;break;
-					case SDLK_1:keyboard[1] = 1;break;
-					case SDLK_2:keyboard[2] = 1;break;
-					case SDLK_3:keyboard[3] = 1;break;
-					case SDLK_q:keyboard[4] = 1;break;
-					case SDLK_w:keyboard[5] = 1;break;
-					case SDLK_e:keyboard[6] = 1;break;
-					case SDLK_a:keyboard[7] = 1;break;
-					case SDLK_s:keyboard[8] = 1;break;
-					case SDLK_d:keyboard[9] = 1;break;
-					case SDLK_z:keyboard[0xA] = 1;break;
-					case SDLK_c:keyboard[0xB] = 1;break;
-					case SDLK_4:keyboard[0xC] = 1;break;
-					case SDLK_r:keyboard[0xD] = 1;break;
-					case SDLK_f:keyboard[0xE] = 1;break;
-					case SDLK_v:keyboard[0xF] = 1;break;
-				}
-				break;
-				
-				case SDL_KEYUP:
-				
-				switch (event.key.keysym.sym)
-				{
-					case SDLK_x:keyboard[0] = 0;break;
-					case SDLK_1:keyboard[1] = 0;break;
-					case SDLK_2:keyboard[2] = 0;break;
-					case SDLK_3:keyboard[3] = 0;break;
-					case SDLK_q:keyboard[4] = 0;break;
-					case SDLK_w:keyboard[5] = 0;break;
-					case SDLK_e:keyboard[6] = 0;break;
-					case SDLK_a: keyboard[7] = 0;break;
-					case SDLK_s:keyboard[8] = 0;break;
-					case SDLK_d:keyboard[9] = 0;break;
-					case SDLK_z:keyboard[0xA] = 0;break;
-					case SDLK_c:keyboard[0xB] = 0;break;
-					case SDLK_4:keyboard[0xC] = 0;break;
-					case SDLK_r:keyboard[0xD] = 0;break;
-					case SDLK_f:keyboard[0xE] = 0;break;
-					case SDLK_v:keyboard[0xF] = 0;break;
+					case SDL_QUIT:
+					quit = 1;
+					break;
+
+					case SDL_KEYDOWN:
+
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_ESCAPE:quit = 1;break;
+						case SDLK_F1:initChip8();loadRom();break;
+						case SDLK_F2:speed -= 1;break;
+						case SDLK_F3:speed += 1;break;
+						case SDLK_x:keyboard[0] = 1;break;
+						case SDLK_1:keyboard[1] = 1;break;
+						case SDLK_2:keyboard[2] = 1;break;
+						case SDLK_3:keyboard[3] = 1;break;
+						case SDLK_q:keyboard[4] = 1;break;
+						case SDLK_w:keyboard[5] = 1;break;
+						case SDLK_e:keyboard[6] = 1;break;
+						case SDLK_a:keyboard[7] = 1;break;
+						case SDLK_s:keyboard[8] = 1;break;
+						case SDLK_d:keyboard[9] = 1;break;
+						case SDLK_z:keyboard[0xA] = 1;break;
+						case SDLK_c:keyboard[0xB] = 1;break;
+						case SDLK_4:keyboard[0xC] = 1;break;
+						case SDLK_r:keyboard[0xD] = 1;break;
+						case SDLK_f:keyboard[0xE] = 1;break;
+						case SDLK_v:keyboard[0xF] = 1;break;
+					}
+					break;
+					
+					case SDL_KEYUP:
+					
+					switch (event.key.keysym.sym)
+					{
+						case SDLK_x:keyboard[0] = 0;break;
+						case SDLK_1:keyboard[1] = 0;break;
+						case SDLK_2:keyboard[2] = 0;break;
+						case SDLK_3:keyboard[3] = 0;break;
+						case SDLK_q:keyboard[4] = 0;break;
+						case SDLK_w:keyboard[5] = 0;break;
+						case SDLK_e:keyboard[6] = 0;break;
+						case SDLK_a: keyboard[7] = 0;break;
+						case SDLK_s:keyboard[8] = 0;break;
+						case SDLK_d:keyboard[9] = 0;break;
+						case SDLK_z:keyboard[0xA] = 0;break;
+						case SDLK_c:keyboard[0xB] = 0;break;
+						case SDLK_4:keyboard[0xC] = 0;break;
+						case SDLK_r:keyboard[0xD] = 0;break;
+						case SDLK_f:keyboard[0xE] = 0;break;
+						case SDLK_v:keyboard[0xF] = 0;break;
+					}
+					break;
 				}
 				break;
 			}
-			break;
-		}
 			if(speed<0){
 				
 				speed = 0;
